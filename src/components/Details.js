1f5c3ecbc77a3c4bb532cdf4bc1e20240ee83892
randi_maizul <rmaizul@gmail.com>
2019-01-24 08:23:50 +0700

    store

---
+++ src/components/Details.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Details extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h3>Hello From Details</h3>
+      </div>
+    );
+  }
+}
+
+export default Details;
