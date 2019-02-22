f7061bf6f467df10d3ee00ddc0272ae5e8d8173d
randi_maizul <rmaizul@gmail.com>
2019-02-22 10:55:06 +0700

    finish

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Tittle from "../Tittle";
+import CartColumns from "./CartColumns";
+import EmptyCart from "./EmptyCart";
+import { ProductConsumer } from "../../context";
+import CartList from "./CartList";
+import CartTotals from "./CartTotals";
+
+export default class Cart extends Component {
+  state = {};
+  render() {
+    return (
+      <section>
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Tittle name="your" tittle="cart" />
+                  <CartColumns />
+                  <CartList value={value} />
+                  <CartTotals value={value} />
+                </React.Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
+      </section>
+    );
+  }
+}
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
 import Product from "./Product";
-
+import Tittle from "./Tittle";
+import { ProductConsumer } from "../context";
 class ProductList extends Component {
-  state = {};
   render() {
     return (
-      <div>
-        <Product />
-      </div>
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Tittle name="our" tittle="products" />
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(product => {
+                    return (
+                      <Product
+                        key={product.id}
+                        product={product}
+                        // handelDetail={value.handelDetail}
+                      />
+                    );
+                  });
+                }}
+              </ProductConsumer>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
